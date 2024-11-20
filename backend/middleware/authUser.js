ba6894f193cabea2780727f57d05ff16620dffe5
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2024-11-20 21:36:07 +0000

    Update authUser.js

--- backend/middleware/authUser.js
+++ backend/middleware/authUser.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 export const userAuth = async(req,res,next)=>{
     try{
-        const token = req.headers.token
+        const {token} = req.body
         if(!token){
             return res.json({success:false, message:"no token"})
         }
