aa2d2da95b4b5ba5f1d140f94969cb0f558bb6f6
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2024-11-27 14:35:14 +0000

    Update userController.js

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -133,8 +133,8 @@ export const updateScores = async(req,res)=>{
 
         balance = currentCoins
         overallCoins+=balance
-        currentToken = (currentCoins/5000).toFixed(3)
-        overallToken = (overallCoins/5000).toFixed(3)
+        currentToken = parseInt((currentCoins/5000).toFixed(3),10)
+        overallToken = parseInt((overallCoins/5000).toFixed(3), 10)
         const scoreUpdate = {overallCoins,currentToken,overallToken,balance}
         console.log(scoreUpdate)
         const updateScores = await userSchema.findByIdAndUpdate(id,{Scores:scoreUpdate},{new:true})
