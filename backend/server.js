ea2f547a12fd54e2c16e4f53815c857e08416864
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2024-12-18 14:29:34 +0000

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cors from "cors"
 import { userRoutes } from "./Routes/userRoutes.js"
 import { pictureRoute } from "./Routes/pictureRoute.js"
+
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 const port = process.env.PORT || 9000
 
 app.listen(port, ()=>{
